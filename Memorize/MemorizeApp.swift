298cc632d2e82de66a5a82443b7b1eff7fc004a1
Arkadii Zuev <arkasha.zuev@icloud.com>
2021-10-26 19:08:44 +0500

    Initial Commit

---
+++ Memorize/MemorizeApp.swift
@@ -0,0 +1,17 @@
+//
+//  MemorizeApp.swift
+//  Memorize
+//
+//  Created by Arkasha Zuev on 26.10.2021.
+//
+
+import SwiftUI
+
+@main
+struct MemorizeApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
